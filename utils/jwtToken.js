aeee55a39b44477bddf853d084fc7bbcf18c012a
Sujal <sharmasujal995@gmail.com>
2024-04-25 18:20:15 +0530

    Initial commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,6 +3,8 @@ export const sendToken = (user, statusCode, message, res) => {
   const options = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 1000),
     httpOnly: true,
+    secure: true,
+    sameSite: "None",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
